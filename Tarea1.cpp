f8eff06fe0a2070e715bf09653f9f57aa6b6c8ee
Andrea Victoria <87445219+4ndreams@users.noreply.github.com>
2024-10-20 17:08:18 -0300

    Update Tarea1.cpp
    Agregar comentarios al código

--- Tarea1.cpp
+++ Tarea1.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <cstdlib>
 
+// Variables globales
 std::mutex mtx;  
 int posicion = 1; 
 
@@ -34,6 +35,7 @@ void carrera_auto(int id, int M) {
         case 5: color = CYAN; break;
     }
 
+    // Avance de los autos
     while (distancia_recorrida < M) {
         int avance = rand() % 10 + 1;
         distancia_recorrida += avance;
@@ -41,6 +43,7 @@ void carrera_auto(int id, int M) {
             distancia_recorrida = M;
         }
 
+        // Imprimir por consola la posición de los autos
         {
             std::lock_guard<std::mutex> guard(mtx);
             for (int i = 0; i <= id; ++i) std::cout << std::endl;
@@ -48,21 +51,26 @@ void carrera_auto(int id, int M) {
             std::cout << color << "Auto " << id +1 << " 🚗 " << distancia_recorrida << "[mt]"<< RESET << std::endl;
         }
 
+        // Esperar un tiempo aleatorio
         std::this_thread::sleep_for(std::chrono::milliseconds(100 + rand() % 401));
     }
 
+        // Imprimir por consola la posición de los autos
     {
         std::lock_guard<std::mutex> guard(mtx);
         std::cout << color << "Auto " << id +1<< " termina la carrera en el lugar " << posicion++ << "!" << RESET << std::endl;
     }
 }
 
+
 int main(int argc, char* argv[]) {
+    
     if (argc != 3) {
         std::cerr << "Uso: " << argv[0] << " <distancia_total> <numero_autos>" << std::endl;
         return 1;
     }
 
+    // Inicializar la carrera
     int M = std::stoi(argv[1]);
     int N = std::stoi(argv[2]);
     std::vector<std::thread> hilos;
@@ -75,6 +83,7 @@ int main(int argc, char* argv[]) {
         hilo.join();
     }
 
+    // Imprimir por consola el término de la carrera
     std::cout << "La carrera ha terminado!" << std::endl;
     return 0;
 }
